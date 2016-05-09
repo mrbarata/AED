d40a8bb683d00947481eef9c35f02e4c588c811d
Miguel Barata <miguelrebelobarata@gmail.com>
2016-05-09 11:54:33 +0100

    Implementation of "getKBiggest()" from "ListUtils". T&W

--- src/series/serie2/ListUtils.java
+++ src/series/serie2/ListUtils.java
@@ -7,15 +7,28 @@ public class ListUtils {
 
 
     public static <E> E getKBiggest(Node<E> list, int k, Comparator<E> cmp) {
-        throw new UnsupportedOperationException();
-    }
+        //throw new UnsupportedOperationException();
+        Node<E> k_biggest = list;
+        int cnt = 0;
+        while(cnt < k && list != null){
+            cnt++;
+            list = list.next;
+        }
+        if(cnt != k || k < 1)  //list has less than k elements
+            return null;
 
+        while(list != null) {
+            k_biggest = k_biggest.next;
+            list = list.next;
+        }
+        return k_biggest.value;
+    }
 
     public static <E> Node<E> merge(Node<E>[] lists, Comparator<E> cmp){
         throw new UnsupportedOperationException();
     }
 
-   public static <E> void internalReverse(Node<Node<E>> list) {
+    public static <E> void internalReverse(Node<Node<E>> list) {
        throw new UnsupportedOperationException();
    }
 
